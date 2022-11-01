394f37cd8c0e3841fef603ed9aed60298a1ef38f
rpaskalev <68789012+rpaskalev@users.noreply.github.com>
2022-10-31 20:31:18 -0400

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "dev_bucket" {
+  bucket = "jenkins-bucket-ziyotek-${data.aws_caller_identity.current.account_id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "dev" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.dev_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+
+}
+
+data "aws_caller_identity" "current" {}
+  
