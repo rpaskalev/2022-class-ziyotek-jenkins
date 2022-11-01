3be3f36ddbeb834b9f1e4207f53966af0cf201c6
rpaskalev <rpaskalev@lomixtech.com>
2022-10-31 21:28:51 -0400

    first jenkins tf build

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,10 +7,10 @@ provider "aws" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "public-tfpstate-rady-bucket"
-    key    = "tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "public-tfpstate-rady-bucket"
+#     key    = "tfstate"
+#     region = "us-east-1"
+#   }
+# }
